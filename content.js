eca2971ebfb76cc48dc9b3ff0357845a6e176b29
Tirth9978 <tirthppatel9978@gmail.com>
2025-07-21 18:52:16 +0530

    I push it

---
+++ content.js
@@ -0,0 +1,10 @@
+
+document.body.textContent = ""
+
+let img = document.createElement("img")
+img.src = "https://media.tenor.com/evOjo3SiS1sAAAAe/cat-blink.png"
+
+document.body.append(img)
+img.style.display ="inline"
+img.backgroundColor = "black"
+img.style.margin = "0 auto"
